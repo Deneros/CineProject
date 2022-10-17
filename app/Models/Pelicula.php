4111ee6255880bf230f685855ef5f1204596d454
Deneros <nicolas.velez99@hotmail.com>
2022-10-17 13:43:27 -0500

    [feat]: creamos modelos

---
+++ app/Models/Pelicula.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Peliculas extends Model
+{
+    use HasFactory;
+
+    protected $table = 'peliculas';
+
+    protected $fillable= [
+        'nombre_pelicula',
+        'descripcion',
+        'genero',
+        'precio',
+        'sala',
+        'tipo',
+        'fecha',
+        'poster'
+    ];
+
+    protected $timestamps = false;
+    
+}
